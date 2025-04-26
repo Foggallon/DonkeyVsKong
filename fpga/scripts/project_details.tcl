5f259a7c0e8c97cfd232b8d674460d691192c513
Dawid <bodzekdawid@gmail.com>
2025-04-26 20:15:37 +0200

    Dodano nowe pliki

---
+++ fpga/scripts/project_details.tcl
@@ -0,0 +1,48 @@
+# Copyright (C) 2025  AGH University of Science and Technology
+# MTM UEC2
+# Author: Dawid Bodzek
+#
+# Description:
+# Project detiles required for generate_bitstream.tcl
+
+#-----------------------------------------------------#
+#                   Project details                   #
+#-----------------------------------------------------#
+# Project name                                  
+set project_name DonkeyVsKong
+
+# Top module name                               
+set top_module top_vga_basys3
+
+# FPGA device
+set target xc7a35tcpg236-1
+
+#-----------------------------------------------------#
+#                    Design sources                   #
+#-----------------------------------------------------#
+# Specify .xdc files location
+set xdc_files {
+    constraints/top_vga_basys3.xdc
+}
+
+# Specify SystemVerilog design files location
+set sv_files {
+    ../rtl/vga_pkg.sv
+    ../rtl/vga_timing.sv
+    rtl/top_vga_basys3.sv
+}
+
+# Specify Verilog design files location
+#set verilog_files {
+#
+#}
+
+# Specify VHDL design files location
+#set vhdl_files {
+#
+#}
+
+# Specify files for a memory initialization
+#set mem_files {
+#
+#}
